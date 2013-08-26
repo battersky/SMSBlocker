b93b11224620d3361205fa86134a1b7c5ced1462
sky batter <batter.sky@gmail.com>
2013-08-26 23:01:03 +0800

    add two new files
    1. SmsBlockerDataService.java which is a service for loading
    and saving data in database
    2.TestActivity.java an activity for test purpose

---
+++ src/com/batter/smsblocker/TestActivity.java
@@ -0,0 +1,16 @@
+package com.batter.smsblocker;
+
+import com.batter.smsblocker.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity {
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
